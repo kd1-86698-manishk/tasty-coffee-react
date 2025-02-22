3775944fbaae534e1d683ee2177752aa7b7893ab
manishk1199 <manishkarne1967@gmail.com>
2025-02-22 14:43:38 +0530

    Home Section completed

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../../components/Layouts/Layout";
+import Section1 from "./Section1";
+import  "../../styles/HomeStyle.css";
+function Home() {
+  return (
+    <>
+      <Layout>
+        {/* Home Section Hero Banner */}
+        <Section1/>
+      </Layout>
+    </>
+  );
+}
+
+export default Home;
